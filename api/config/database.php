9129114720a7ff184e482fa163df9e56e9eee704
jayshrikamble635-sudo <jayshrikamble635@gmail.com>
2026-09-21 15:23:25 +0530

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -7,9 +7,8 @@ $database = "digiverify";
 
 $conn = new mysqli($host, $user, $password, $database);
 
-if ($conn->connect_error) {
-    die(json_encode([
-        "status" => false,
-        "message" => "Database Connection Failed: " . $conn->connect_error
-    ]));
+if($conn->connect_error){
+    die("Database Connection Failed : " . $conn->connect_error);
 }
+
+?>
