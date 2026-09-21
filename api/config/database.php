9b9456fec186590a88eacca7931bb42c8925ae5c
jayshrikamble635-sudo <jayshrikamble635@gmail.com>
2026-09-21 15:50:37 +0530

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -3,7 +3,7 @@
 // Clever Cloud लाइव डेटाबेस क्रेडेंशियल्स
 $host = "://clever-cloud.com";
 $user = "bnljgn27equjadrmm6w7";
-$password = "यहाँ_अपना_CLEVER_CLOUD_PASSWORD_पेस्ट_करें"; // <-- यहाँ अपना असली पासवर्ड डालें
+$password = "अपना_पासवर्ड_यहाँ_पेस्ट_करें"; // <-- उद्धरण चिह्नों (' ') के बीच में अपना असली पासवर्ड पेस्ट करें
 $database = "bnljgn27equjadrmm6w7";
 
 $conn = new mysqli($host, $user, $password, $database);
