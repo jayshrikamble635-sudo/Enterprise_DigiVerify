11464919a0438087dcc78383a907e840d708ad94
jayshrikamble635-sudo <jayshrikamble635@gmail.com>
2026-09-21 18:10:36 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,14 +1,19 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "digiverify";
+// Clever Cloud लाइव डेटाबेस क्रेडेंशियल्स 
+$host = "://clever-cloud.com";
+$user = "usmmcxltshqjsde2";
+$password = "4yIROXJGxupdTdzB6dZm";
+$database = "bnljgn27equjadrmm6w7";
 
-$conn = new mysqli($host, $user, $password, $database);
+// बिना किसी चेतावनी के सीधे MySQL से कनेक्शन बनाना
+$conn = @new mysqli($host, $user, $password, $database);
 
-if($conn->connect_error){
+if ($conn->connect_error) {
     die("Database Connection Failed : " . $conn->connect_error);
 }
+
+// यह कनेक्शन को लाइव सर्वर के लिए एकदम पक्का (Strict) कर देगा
+$conn->set_charset("utf8mb4");
 
 ?>
