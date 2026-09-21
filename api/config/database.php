5c4b93ef6b4fcc3f9847c9e5eaf5e88ddf1fd837
jayshrikamble635-sudo <jayshrikamble635@gmail.com>
2026-09-21 15:55:13 +0530

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -1,14 +1,15 @@
 <?php
 
-// Clever Cloud लाइव डेटाबेस क्रेडेंशियल्स
+// Clever Cloud लाइव डेटाबेस क्रेडेंशियल्स (बिना किसी स्पेस के पूरा एड्रेस)
 $host = "://clever-cloud.com";
 $user = "usmmcxltshqjsde2";
 $password = "4yIROXJGxupdTdzB6dZm";
 $database = "bnljgn27equjadrmm6w7";
 
-$conn = new mysqli($host, $user, $password, $database);
+// कनेक्शन बनाते समय एरर हैंडलिंग को बेहतर करना
+$conn = @new mysqli($host, $user, $password, $database);
 
-if($conn->connect_error){
+if ($conn->connect_error) {
     die("Database Connection Failed : " . $conn->connect_error);
 }
 
