299063d875ff3403a65dd0103ad94adce452c731
jayshrikamble635-sudo <jayshrikamble635@gmail.com>
2026-09-21 23:05:09 +0530

    Update upload.php

--- verify/upload.php
+++ verify/upload.php
@@ -8,7 +8,7 @@ $message_class = "";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['document_file'])) {
     
-    // 🔥 Clever Cloud MySQL Live Connection
+    // 🔥 Clever Cloud MySQL Live Connection (अब बिल्कुल सही होस्ट के साथ)
     $conn = @new mysqli(
         "://clever-cloud.com", 
         "usmmcxltshqjsde2", 
